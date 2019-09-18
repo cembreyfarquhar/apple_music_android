1b4d8a6c83d4d2e407c509989fac3a358f0cbccb
halo <cembreyfarquhar@gmail.com>
2019-09-17 19:24:28 -0500

    OkHttp bundled successfully - success function appears to be triggering

---
+++ app/src/main/java/me/chancehalo/musicfeed/MainActivity.kt
@@ -0,0 +1,38 @@
+package me.chancehalo.musicfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fetchMusicData()
+    }
+
+    private fun fetchMusicData() {
+        Log.i("dev", "attempting to fetch data")
+        val url =
+            "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/25/explicit.json"
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("dev", "got em!")
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.i("dev", "it failed")
+            }
+        })
+
+    }
+}
