6071065ecc98b82db22ec0be9ecf5a0ffd6f76b0
halo <cembreyfarquhar@gmail.com>
2019-09-19 14:54:39 -0500

    adjusting layout, also adds back.xml drawable for a debugging border

--- app/src/main/java/me/chancehalo/musicfeed/MainActivity.kt
+++ app/src/main/java/me/chancehalo/musicfeed/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import org.json.JSONObject
